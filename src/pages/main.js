b456f0221fb8741f52fa312943cb1bbaaf47b2be
adultcho <moon1601@icloud.com>
2022-06-18 20:43:18 +0900

    6월 18일 20:40 main view 구성. 로그인, 회원가입 모달 창, 서버 통신 확인

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,30 +1,25 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import Modal from "../components/Modal";
+
+//components
+import Card from "../components/Card";
+
+//styles
+import "../styles/pages/Main.css";
 
 const Main = () => {
-  const navigate = useNavigate();
-
-  const moveToDetail = () => {
-    navigate("/detail");
-  };
-
-  const [modalOpen, setModalOpen] = React.useState(false);
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
   return (
     <div className="main">
-      <h1>main page</h1>
-      <button onClick={moveToDetail}>move detail page</button>
-      <button onClick={openModal}>모달팝업</button>
-
-      <Modal open={modalOpen} close={closeModal} header="Modal heading"></Modal>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
