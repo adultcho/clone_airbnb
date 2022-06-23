80641c4c925e61e7d293411d0f583996df36f085
JongHoonHong <hgh275@gmail.com>
2022-06-23 18:29:50 +0900

    no message

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
@@ -20,59 +20,109 @@ import KayakingIcon from "@mui/icons-material/Kayaking";
 import WbShadeIcon from "@mui/icons-material/WbShade";
 
 //  무한 스크롤
-import { List, AutoSizer } from "react-virtualized";
+import { SecurityUpdateGood } from "@mui/icons-material";
 
 const Main = () => {
+  //  observer
+
   const dispatch = useDispatch();
 
   const PostList = useSelector((state) => state.post.list);
   const [category, setCategory] = useState("섬");
-  const selectCategory = (e) => {
-    setCategory(e.target.value);
-  };
+
   React.useEffect(() => {
     dispatch(loadPostDB(category));
   }, [category]);
-
+  console.log(category);
   return (
     <>
       <div className="category_box">
-        <button value="섬" onClick={selectCategory}>
+        <button
+          value="섬"
+          onClick={() => {
+            setCategory("섬");
+          }}
+        >
           <span class="material-symbols-outlined">houseboat</span>섬
         </button>
-        <button value="국립공원" onClick={selectCategory}>
+        <button
+          value="국립공원"
+          onClick={() => {
+            setCategory("국립공원");
+          }}
+        >
           <ForestIcon />
           국립공원
         </button>
-        <button value="통나무집" onClick={selectCategory}>
+        <button
+          value="통나무집"
+          onClick={() => {
+            setCategory("통나무집");
+          }}
+        >
           <CabinIcon />
           통나무집
         </button>
-        <button value="기상천외한숙소" onClick={selectCategory}>
+        <button
+          value="기상천외한숙소"
+          onClick={() => {
+            setCategory("기상천외한숙소");
+          }}
+        >
           <VillaIcon />
           기상천외한숙소
         </button>
-        <button value="해변근처" onClick={selectCategory}>
+        <button
+          value="해변근처"
+          onClick={() => {
+            setCategory("해변근처");
+          }}
+        >
           <BeachAccessIcon />
           해변근처
         </button>
-        <button value="초소형주택" onClick={selectCategory}>
+        <button
+          value="초소형주택"
+          onClick={() => {
+            setCategory("초소형주택");
+          }}
+        >
           <span class="material-symbols-outlined">gite</span>
           초소형주택
         </button>
-        <button value="디자인" onClick={selectCategory}>
+        <button
+          value="디자인"
+          onClick={() => {
+            setCategory("디자인");
+          }}
+        >
           <WbShadeIcon />
           디자인
         </button>
-        <button value="캠핑장" onClick={selectCategory}>
+        <button
+          value="캠핑장"
+          onClick={() => {
+            setCategory("캠핑장");
+          }}
+        >
           <span class="material-symbols-outlined">camping</span>
           캠핑장
         </button>
-        <button value="A자형주택" onClick={selectCategory}>
+        <button
+          value="A자형주택"
+          onClick={() => {
+            setCategory("A자형주택");
+          }}
+        >
           <BungalowIcon />
           A자형주택
         </button>
-        <button value="호숫가" onClick={selectCategory}>
+        <button
+          value="호숫가"
+          onClick={() => {
+            setCategory("호숫가");
+          }}
+        >
           <KayakingIcon />
           호숫가
         </button>
