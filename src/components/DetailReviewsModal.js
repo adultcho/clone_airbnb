158340674feab033f774deff70e4424f18adb7a1
adultcho <moon1601@icloud.com>
2022-06-23 23:31:33 +0900

    6월23일 목 23:31 매니저님 피드백

--- src/components/DetailReviewsModal.js
+++ src/components/DetailReviewsModal.js
@@ -20,9 +20,11 @@ import SearchIcon from "@mui/icons-material/Search";
 import StarIcon from "@mui/icons-material/Star";
 
 function DetailReviewsModal({ openModalCallBack, detailScore }) {
-  const commentList = useSelector((state) => state.comment.list.comments);
+  const commentList = useSelector((state) => state.comment.list);
   console.log(commentList);
   console.log(detailScore);
+
+  const userEmail = localStorage.getItem("user-email");
 
   //postId useParams로 가져오기
   const { postId } = useParams();
@@ -114,7 +116,8 @@ function DetailReviewsModal({ openModalCallBack, detailScore }) {
         <div className="detail_reviews_modal_contents">
           <div className="detail_reviews_modal_left">
             {data
-              ?data.category&& data.category.map((l, index) => {
+              ? data.category &&
+                data.category.map((l, index) => {
                   return (
                     <div className="full">
                       <DetailReviewsScoreCategory
@@ -177,26 +180,30 @@ function DetailReviewsModal({ openModalCallBack, detailScore }) {
                     </div>
                     <div className="detail_reviews_comment_bottom_full">
                       {commentList.comment}
-                      <div>
-                        <button
-                          onClick={() => {
-                            // console.log(commentList.commentId)
-                            const commentId = commentList.commentId;
-                            dispatch(updateCommentDB(postId, commentId));
-                          }}
-                        >
-                          수정
-                        </button>
-                        <button
-                          onClick={() => {
-                            // console.log(commentList.commentId)
-                            const commentId = commentList.commentId;
-                            dispatch(deleteCommentDB(postId, commentId));
-                          }}
-                        >
-                          삭제
-                        </button>
-                      </div>
+                      {userEmail === commentList.useremail ? (
+                        <div>
+                          <button
+                            className="detail_reviews_comment_button"
+                            onClick={() => {
+                              // console.log(commentList.commentId)
+                              const commentId = commentList.commentId;
+                              dispatch(updateCommentDB(postId, commentId));
+                            }}
+                          >
+                            수정
+                          </button>
+                          <button
+                            className="detail_reviews_comment_button"
+                            onClick={() => {
+                              // console.log(commentList.commentId)
+                              const commentId = commentList.commentId;
+                              dispatch(deleteCommentDB(postId, commentId));
+                            }}
+                          >
+                            삭제
+                          </button>
+                        </div>
+                      ) : null}
                     </div>
                   </div>
                 );
